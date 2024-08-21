ec26b02577106264db2ddc1e8c98e4eca108b759
Actsys 10 <Actsys 10@DESKTOP-Q76TTOQ>
2024-08-21 13:50:59 +0530

    addProduct repo

---
+++ src/main/java/xom/shop/dao/Product_Repository.java
@@ -0,0 +1,22 @@
+package xom.shop.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import xom.shop.persist.Product;
+
+@Repository("product_Repo")
+public class Product_Repository {
+
+	@Autowired
+	JdbcTemplate jdbc;
+	@Autowired
+	HibernateTemplate hibernate;
+	
+	public void saveProduct(Product product) {
+		System.out.println("save product :" +product);
+//		hibernate.save(product);
+	}
+}
